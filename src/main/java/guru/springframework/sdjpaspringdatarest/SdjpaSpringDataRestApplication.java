bf5bbac0eb88bc7f41204ed4ff1d7c95dd15b670
SaulGoodman-jl <2246515235@qq.com>
2024-07-01 11:49:22 +0800

    Initial commit

---
+++ src/main/java/guru/springframework/sdjpaspringdatarest/SdjpaSpringDataRestApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sdjpaspringdatarest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaSpringDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaSpringDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/sdjpaspringdatarest/SdjpaSpringDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sdjpaspringdatarest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdjpaSpringDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
